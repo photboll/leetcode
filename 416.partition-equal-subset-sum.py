cad73abbb3b54810f2a4f05230da95b4c5855204
Philip Andersson <photboll@gmail.com>
2025-04-07 10:12:58 +0200

    Completed daily 416

--- 416.partition-equal-subset-sum.py
+++ 416.partition-equal-subset-sum.py
@@ -7,6 +7,21 @@
 # @lc code=start
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        #
+        #if the sum of nums is odd then it is impossible
+        #Otherwise we need to find a subset of the array that is equal to sum // 2 
+        #Knapsack dp 
+        tot = sum(nums)
+        if tot % 2 == 1:
+            return False
+        
+        target = tot // 2
+        #dp[i] is true if it is possible to create a sum of i with the subsets 
+        dp = [False] *(target +1)
+        dp[0] = True
+        for num in nums:
+            for i in range(target, num-1, -1):
+                dp[i] |= dp[i-num]
+        #print(dp)
+        return dp[-1]
 # @lc code=end
 
