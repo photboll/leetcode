aa0a745dca8c793e7f3e65271d37e15c499215b5
Philip Andersson <photboll@gmail.com>
2025-04-13 23:23:53 +0200

    Completed 344

--- 1922.count-good-numbers.py
+++ 1922.count-good-numbers.py
@@ -14,6 +14,6 @@ class Solution:
         #Leading zeros are allowed so
         even_positions = (n + 1) // 2
         odd_positions = n // 2
-        return (pow(5, even_positions, MOD) * pow(4, odd_positions, MOD)) % MOD
+        return (pow(5, even_positions, MOD) * pow(4, odd_positions, MOD)) % MOe
 # @lc code=end
 
