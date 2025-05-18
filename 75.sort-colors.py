fbd2a30914b57d971f4cfb8f45fe5e73570f0dea
Philip Andersson <photboll@gmail.com>
2025-05-18 14:52:16 +0200

    Completed improved version

--- 75.sort-colors.py
+++ 75.sort-colors.py
@@ -7,6 +7,23 @@
 # @lc code=start
 from collections import Counter
 class Solution:
+    def sortColors(self, nums):
+        MID = 1
+        i = 0
+        j = 0
+        k = len(nums) -1
+        while j <= k:
+            if nums[j] < MID:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+            elif nums[j] > MID:
+                nums[j], nums[k] = nums[k], nums[j]
+                k -= 1
+            else:
+                j += 1
+        
+class SolutionV1:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
