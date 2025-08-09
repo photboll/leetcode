9872c00aef6ca04260b19446c0b7e6b7a265d4b8
Philip Andersson <photboll@gmail.com>
2025-08-09 11:50:24 +0200

    Completed Daily

--- 231.power-of-two.py
+++ 231.power-of-two.py
@@ -7,6 +7,9 @@
 # @lc code=start
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and (n & (n -1)) == 0
+class SolutionV1:
+    def isPowerOfTwo(self, n: int) -> bool:
 
         for i in range(32):
             num = (1 << i)
