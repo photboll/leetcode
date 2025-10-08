fcfd65927e39d4acb9f0958c82d2b5107d1c5a4c
Philip <photboll@gmail.com>
2025-10-08 18:33:33 +0200

    Completed Daily

--- 2300.successful-pairs-of-spells-and-potions.py
+++ 2300.successful-pairs-of-spells-and-potions.py
@@ -5,8 +5,30 @@
 #
 
 # @lc code=start
+import bisect
+from fractions import Fraction
+
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        """
+        Binary search the potions list to find the
+        smallest potion that passess. potions[success_min]
+        if potions is sorted then we know that all potions 
+        """
+        n = len(potions)
+        potions.sort()
+        res = []
+
+        for spell in spells:
+            frac = Fraction(success, spell)
+            min_success = bisect.bisect_left(potions, frac)
+            #print(frac, min_success)
+            res.append(n - min_success)
+        return res
+
+
+
+
         
 # @lc code=end
 
