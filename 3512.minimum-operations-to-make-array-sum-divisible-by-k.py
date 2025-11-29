87e6f7d41b4c65bf0b4c70fcb01f7b71476578c6
Philip <photboll@gmail.com>
2025-11-29 09:46:08 +0100

    Completed Daily

---
+++ 3512.minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=3512 lang=python3
+#
+# [3512] Minimum Operations to Make Array Sum Divisible by K
+#
+
+# @lc code=start
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        return abs(sum(nums) % k)
+        
+# @lc code=end
+
