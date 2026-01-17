3d380d85309e4aaf4af8c34583e466e467187a79
Philip <photboll@gmail.com>
2026-01-17 12:45:55 +0100

    Completed 223 V2

--- 223.rectangle-area.py
+++ 223.rectangle-area.py
@@ -10,6 +10,41 @@ def swap(ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, b
 
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        
+        def interval_overlap(a1, a2, b1, b2):
+            lo = max(a1, b1)
+            hi = min(a2, b2)
+            if lo >= hi:
+                return None
+            return lo, hi
+        
+        def rectangle_overlap(ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int):
+            x_interval = interval_overlap(ax1, ax2, bx1, bx2)
+            if x_interval is None:
+                return None
+
+            y_interval = interval_overlap(ay1, ay2, by1, by2)
+            if y_interval is None:
+                return None
+            
+            return x_interval[0], y_interval[0], x_interval[1], y_interval[1]
+        
+        def rectangle_area(x1: int, y1: int, x2: int, y2: int):
+            width = x2 - x1
+            height = y2 - y1
+            return width * height
+        
+        area_a = rectangle_area(ax1, ay1, ax2, ay2)
+        area_b = rectangle_area(bx1, by1, bx2, by2)
+        overlap = rectangle_overlap(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)
+        if overlap is None:
+            overlapping_area = 0
+        else:
+            overlapping_area = rectangle_area(*overlap)
+        return area_a + area_b - overlapping_area
+    
+class SolutionV1:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
         """  
         First we consider 1D, overlapping intervals,
         Do i want to relabel them in anyway, like a always being the leftmost
