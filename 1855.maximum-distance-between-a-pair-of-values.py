2339171ab1b5151f0ada3ae8a567b28e56c5880b
Philip <photboll@gmail.com>
2026-04-19 09:32:22 +0200

    Completed Daily

--- 1855.maximum-distance-between-a-pair-of-values.py
+++ 1855.maximum-distance-between-a-pair-of-values.py
@@ -7,6 +7,35 @@
 # @lc code=start
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        n = len(nums1)
+        m = len(nums2)
+        result = 0
+        def bisect_right_desc(arr, target):
+            lo, hi = 0, len(arr)
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if arr[mid] >= target:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            return lo
+
+        for i in range(n):  
+
+            k = bisect_right_desc(nums2, nums1[i])
+            print(f"{i, k-1}, {nums1[i], nums2[k-1]}")
+
+            result = max(result, 
+                        k - i -1
+                         )
+        return result
+            
+
+
+        
+        
+
+
         
 # @lc code=end
 
